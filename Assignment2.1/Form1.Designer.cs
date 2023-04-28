33ca023875aa36319905debcf16aeef50dc4173b
Xanbertis <Xanbertis_Pym@gmx.fr>
2023-04-28 19:50:44 +0200

    #5 Added choose File button and dialog

--- Assignment2.1/Form1.Designer.cs
+++ Assignment2.1/Form1.Designer.cs
@@ -29,13 +29,39 @@ namespace Assignment2._1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 33);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Choose File...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 415);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- Assignment2.1/Form1.cs
+++ Assignment2.1/Form1.cs
@@ -16,5 +16,22 @@ namespace Assignment2._1
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set the file dialog properties
+            openFileDialog2.Filter = "User Form Infos (*.ufi)|*.ufi|Text files (*.txt)|*.txt";
+            openFileDialog2.Title = "Select a User Infos file";
+
+            // Show the dialog and process the result
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                // The user selected a file, do something with it
+                string selectedFilePath = openFileDialog2.FileName;
+                // ... do something with the selectedFilePath ...
+            }
+        }
     }
 }
