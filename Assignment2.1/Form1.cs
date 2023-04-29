bdd4bc09e259f50b0cf306a9c9987546303e65dd
Xanbertis <Xanbertis_Pym@gmx.fr>
2023-04-29 12:07:52 +0200

    #3 Implemented the file parser class and semi-empty user and UserDisplayer class

--- Assignment2.1/Form1.cs
+++ Assignment2.1/Form1.cs
@@ -64,4 +64,70 @@ namespace Assignment2._1
             return content;
         }
     }
+
+    class UserParser
+    {
+        public string Raw_file_data;
+        public List<Dictionary<string, string>> Normalized_data { get; set; }
+
+
+        public UserParser(string raw_data)
+        {
+            Raw_file_data = raw_data;
+            Normalized_data = new List<Dictionary<string, string>>();
+
+            // Split the raw data into lines
+            string[] lines = raw_data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Iterate over the lines and split each line into key-value pairs
+            foreach (string line in lines)
+            {
+                // Split the line into key-value pairs
+                string[] parts = line.Split('=');
+
+                // Create a new dictionary to store the key-value pair
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+
+                // Add the key-value pair to the dictionary
+                dict.Add(parts[0].Trim(), parts[1].Trim());
+
+                // Add the dictionary to the Normalized_data list
+                Normalized_data.Add(dict);
+            }
+        }
+
+        public void CreateUser(string user_data)
+        {
+            // Split the user data into separate fields
+            string[] fields = user_data.Split(';');
+
+            // Create a new User object with the fields
+            User user = new User(Normalized_data);
+        }
+
+        public void StoreUserInfos()
+        {
+            // Create a new instance of UserDisplayer
+            UserDisplayer userDisplayer = new UserDisplayer();
+        }
+    }
+
+    class User
+    {
+        public List<Dictionary<string, string>> normalized_data;
+
+        public User(List<Dictionary<string, string>> normalized_data)
+        {
+            this.normalized_data = normalized_data;
+        }
+    }
+
+    class UserDisplayer
+    {
+        public void DisplayUserInfo(string[] user_data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
