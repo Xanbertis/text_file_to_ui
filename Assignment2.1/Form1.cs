586e82b529d157e018b13eb094ffd69109777c9a
Xanbertis <Xanbertis_Pym@gmx.fr>
2023-04-30 12:41:01 +0200

    #4 Move UserDisplayer class to its own file

--- Assignment2.1/Form1.cs
+++ Assignment2.1/Form1.cs
@@ -77,33 +77,6 @@ namespace Assignment2._1
         private void textBox6_TextChanged(object sender, EventArgs e) { }
     }
 
-    class UserDisplayer
-    {
-        public List<Dictionary<string, string>> Input_data;
-        public User User_to_display;
-
-        public UserDisplayer(List<Dictionary<string, string>> normalized_data)
-        {
-            this.Input_data = normalized_data;
-        }
-
-        public UserInfoForm createForm()
-        {
-            UserInfoForm form = new UserInfoForm();
-
-            // Add each key-value pair to the form
-            foreach (Dictionary<string, string> dict in Input_data)
-            {
-                foreach (KeyValuePair<string, string> kvp in dict)
-                {
-                    form.addUserInfo(kvp.Key, kvp.Value);
-                }
-            }
-
-            return form;
-        }
-    }
-
     class UserInfoForm
     {
         public string Name;
---
+++ Assignment2.1/UserDisplayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._1
+{
+    class UserDisplayer
+    {
+        public List<Dictionary<string, string>> Input_data;
+        public User User_to_display;
+
+        public UserDisplayer(List<Dictionary<string, string>> normalized_data)
+        {
+            this.Input_data = normalized_data;
+        }
+
+        public UserInfoForm createForm()
+        {
+            UserInfoForm form = new UserInfoForm();
+
+            // Add each key-value pair to the form
+            foreach (Dictionary<string, string> dict in Input_data)
+            {
+                foreach (KeyValuePair<string, string> kvp in dict)
+                {
+                    form.addUserInfo(kvp.Key, kvp.Value);
+                }
+            }
+
+            return form;
+        }
+    }
+}
